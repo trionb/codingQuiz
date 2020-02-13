bde90a93aee75fc6e8711a82aa60bd66d5eec6c3
trionb <trionbailey@gmail.com>
2020-02-13 18:18:32 -0500

    script.js update

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let startBtn = document.getElementById("start");
 
 let startText = document.getElementById("note");
+
+let nextElement = document.getElementById("nextbtn")
 //  let startQuiz=document.getElementById("start quiz");
 
 // let choice0 = document.getElementById("choiceA");
@@ -23,115 +25,164 @@ function startQuiz() {
     } else {
         x.style.display = "none";
     }
-     
+
 
 }
 let questionElement = document.getElementById("questions");
+let timerElement = document.getElementById("time");
+
+let timer = 60;
+let stopTimer = 00
 //console.log(questionElement);
+
 
 startBtn.addEventListener("click", startClickHandler);
 
 function startClickHandler(event) {
     //console.log(event.target);
-    //displayNextQuestions();
 
 
-//make a function for questions to be shown
+    //make a function for questions to be shown
 
-let questions = [
-    {
-        question: " Who said, Ask any racer, any real racer,it doesn't matter if you win by an inch or a mile, winning is winning?",
-        choiceA: "Vince",
-        choiceB: "Leon",
-        choiceC: "Hector",
-        choiceD: "Dom",
-        answer: 3
-    },
-    {
-        question: "Who drove the orange lamborghini mercielago on the ice?",
+    let questions = [
+        {
+            question: " Who said, Ask any racer, any real racer,it doesn't matter if you win by an inch or a mile, winning is winning?",
+            choiceA: "Vince",
+            choiceB: "Leon",
+            choiceC: "Hector",
+            choiceD: "Dom",
+            answer: "Dom"
+        },
+        {
+            question: "Who drove the orange lamborghini mercielago on the ice?",
+            choiceA: "The Rock",
+            choiceB: "Jason Statham",
+            choiceC: "VIn Disel",
+            choiceD: "Tyrese",
+            answer: "Tyrese"
+        },
+        {
+            question: "What is the beer of choice in the movie?",
+            choiceA: "Budweiser",
+            choiceB: "Corona",
+            choiceC: "Blue Moon",
+            choiceD: "Coors Light",
+            answer: "Corona"
+        },
+        {
+            question: "What was the car of choice for Dom",
+            choiceA: "Dodge Charger",
+            choiceB: "Mustang gt350",
+            choiceC: "Toyota Supra",
+            choiceD: "Honda civi Si",
+            answer: "Dodge Charger"
+        },
+        {
+            question: "What is Brian's true profession?.",
+            choiceA: "chef",
+            choiceB: "accountant",
+            choiceC: "cop",
+            choiceD: "mechanic",
+            answer: "cop"
+        },
+    ];
 
-        choiceA: "The Rock",
-        choiceB: "Jason Statham",
-        choiceC: "VIn Disel",
-        choiceD: "Tyrese",
-        answer: 3
-    },
-    {
-        question: "What is the beer of choice in the movie?",
-        choiceA: "Budweiser",
-        choiceB: "Corona",
-        choiceC: "Blue Moon",
-        choiceD: "Coors Light",
-        answer: 1
-    },
-    {
-        question: "What was the car of choice for Dom",
-        choiceA: "Dodge Charger",
-        choiceB: "Mustang gt350",
-        choiceC: "Toyota Supra",
-        choiceD: "Honda civi Si",
-        answer: 0
-    },
-    {
-        question: "What is Brian's true profession?.",
-        choiceA: "chef",
-        choiceB: "accountant",
-        choiceC: "cop",
-        choiceD: "mechanic",
-        answer: 2
-    },
-];
-let questionIndex = 0;
+                for( let i = 0; i< questions.length; questions);
 
-function displayNextQuestions() {
-}
-let currentQuestion = questions[questionIndex];
 
-let questionDiv = document.createElement('div');
-questionDiv.textContent = currentQuestion.question;
+    let questionIndex = 0;
 
-let choiceADiv = document.createElement('div');
-choiceADiv.textContent = currentQuestion.choiceA;
+    function displayNextQuestions() {
 
-let choiceBDiv = document.createElement('div');
-choiceBDiv.textContent = currentQuestion.choiceB;
+        let currentQuestion = questions[questionIndex];
 
-let choiceCDiv = document.createElement('div');
-choiceCDiv.textContent = currentQuestion.choiceC;
+        let questionDiv = document.createElement('div');
+        questionDiv.textContent = currentQuestion.question;
 
-let choiceDDiv = document.createElement('div');
-choiceDDiv.textContent = currentQuestion.choiceD;
+        let choiceADiv = document.createElement('button');
+        choiceADiv.textContent = currentQuestion.choiceA;
 
-let answerDiv = document.createElement('div');
-answerDiv.textContent = currentQuestion.answer;
+        let choiceBDiv = document.createElement('button');
+        choiceBDiv.textContent = currentQuestion.choiceB;
 
-}
-//set timer to countdown when quiz is started
-let timer = document.getElementById("time");
-    function startTimer(duration, display) {
-    let timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+        let choiceCDiv = document.createElement('button');
+        choiceCDiv.textContent = currentQuestion.choiceC;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        let choiceDDiv = document.createElement('button');
+        choiceDDiv.textContent = currentQuestion.choiceD;
 
-        display.textContent = minutes + ":" + seconds;
+        // let answerDiv = document.createElement('div');
+        // answerDiv.textContent = currentQuestion.answer;
 
-        if (--timer < 0) {
-            timer = duration;
+        questionElement.appendChild(questionDiv);
+        questionElement.appendChild(choiceADiv);
+        questionElement.appendChild(choiceBDiv);
+        questionElement.appendChild(choiceCDiv);
+        questionElement.appendChild(choiceDDiv);
+
+        choiceADiv.addEventListener("click", function () {
+            if (questions[questionIndex].answer === this.textContent) {
+                alert("correct")
+            } else {
+                alert("wrong");
+            }
+        });
+
+        choiceBDiv.addEventListener("click", function () {
+            if (questions[questionIndex].answer === this.textContent) {
+                alert("correct")
+            } else {
+                alert("wrong");
+            }
+        });
+
+        choiceCDiv.addEventListener("click", function () {
+            if (questions[questionIndex].answer === this.textContent) {
+                alert("correct")
+            } else {
+                alert("wrong");
+            }
+        });
+
+        choiceDDiv.addEventListener("click", function () {
+            if (questions[questionIndex].answer === this.textContent) {
+                alert("correct")
+            } else {
+                alert("wrong");
+            }
+        });
+        
+        //set timer to countdown when quiz is started
+        nextElement.addEventListener("click", nextQuiz);
+        function nextQuiz() {
+            nextElement.classList.add("hide")
+            var x = document.getElementById("questions");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
         }
-    }, 1000);
+
+    }
+    displayNextQuestions()
+
+
+    function startTimer() {
+        var timerInterval = setInterval(function () {
+            (timer--)
+            timerElement.textContent = "0:" + timer;
+        }, 1000);
+        //if(timerInterval === stopTimer)
+
+        // if ( minutes=== 0 && seconds === 0) {
+        //     clearInterval(timerInterval);
+        //     callback && callback();
+
+    }
+
+    startTimer();
 }
-
-window.onload = function () {
-    var oneMinute = 60 * 1,
-        display = document.querySelector('#time');
-    startTimer(oneMinute, display);
-};
-
-
 
 
 
@@ -193,4 +244,3 @@ window.onload = function () {
 // }
 // localStorage.setItem("questions", score);
 // console.log(localStorage);
-
